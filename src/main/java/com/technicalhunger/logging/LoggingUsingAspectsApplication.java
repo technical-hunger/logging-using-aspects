af2ace6d43fa3ad9af4d6f5266f07805129a30a6
Shahnavaz Saiyad <ssaiyed173@gmail.com>
2022-03-19 14:30:38 +0530

    Initial springboot project

---
+++ src/main/java/com/technicalhunger/logging/LoggingUsingAspectsApplication.java
@@ -0,0 +1,13 @@
+package com.technicalhunger.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingUsingAspectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingUsingAspectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technicalhunger/logging/LoggingUsingAspectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technicalhunger.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingUsingAspectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
